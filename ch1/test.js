40b90360063117caaf1ace6d29d3f6d923d6235c
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2020-09-06 15:48:20 +0900

    1.2.5

---
+++ ch1/test.js
@@ -0,0 +1,9 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.test = void 0;
+    function test() {
+        return 'test';
+    }
+    exports.test = test;
+});
