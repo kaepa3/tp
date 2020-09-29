9725b07f6cec1b10df8924de6410b1673363248c
山崎孝洋 <yamazakitakahiro@yamazakitakahironoMacBook-Pro.local>
2020-09-29 19:36:56 +0900

    until 7-1

---
+++ ch7/parcel/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { rows } from './data'
+import Thead from './thread'
+import Tbody from './tbody'
+
+const Component: React.FC = () =>(
+    <div>
+    <h1>健康に関する調査</h1>
+        <table>
+        <Thead />
+        <Tbody rows ={rows} />
+        </table>
+    </div>
+)
+
+export default Component
+
